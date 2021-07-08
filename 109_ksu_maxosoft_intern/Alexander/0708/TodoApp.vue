b705921f6f8b8e36845f56afb946eec02e80a62a
4070E019 <43432169+4070E019@users.noreply.github.com>
2021-07-08 15:22:57 +0800

    Add files via upload

---
+++ 109_ksu_maxosoft_intern/Alexander/0708/TodoApp.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="container" style="max-width: 600px">
+    <!-- Heading -->
+    <h2 class="text-center mt-5">我的清單</h2>
+
+    <!-- Input -->
+    <div class="d-flex mt-5">
+      <input
+        type="text"
+        v-model="task"
+        placeholder="請輸入待辦事項"
+        class="w-100 form-control"
+      />
+      <button class="btn btn-warning rounded-0" @click="submitTask"
+      >
+        SUBMIT
+      </button>
+      
+    </div>
+
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">事情</th>
+          <th scope="col" style="width: 120px">狀態</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ 'line-through': task.status === '完成' }">
+              {{ task.name }}
+            </span>
+          </td>
+          <td>
+            <span
+              class="pointer noselect"
+              @click="changeStatus(index)"
+              :class="{
+                'text-danger': task.status === '完成',
+                'text-success': task.status === '待辦',
+                'text-warning': task.status === '正在做',
+              }"
+            >                                                                                                                          
+              {{ capitalizeFirstChar(task.status) }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+          <td class="text-center">
+            <div @click="editTask(index)">
+              <p class="fa fa-pen pointer"></p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      task: "",
+      editedTask: null,
+      statuses: ["待辦", "正在做", "完成"],
+      /* Status could be: 'to-do' / 'in-progress' / 'finished' */
+      tasks: [
+        {
+          name: "買咖啡",
+          status: "待辦",
+        },
+        {
+          name: "休息",
+          status: "正在做",
+        },
+        {
+          name: "掃地",
+          status: "完成",
+        },
+      ],
+    };
+  },
+  methods: {
+    /**
+     * Capitalize first character
+     */
+    capitalizeFirstChar(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+    /**
+     * Change status of task by index
+     */
+    changeStatus(index) {
+      let newIndex = this.statuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.statuses[newIndex];
+    },
+    /**
+     * Deletes task by index
+     */
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    /**
+     * Edit task
+     */
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+    /**
+     * Add / Update task
+     */
+    submitTask() {
+      if (this.task.length === 0) return;
+      /* We need to update the task */
+      if (this.editedTask != null) {
+       this.tasks[this.editedTask].name = this.task;
+  
+        this.editedTask = null;
+      } else {
+        /* We need to add new task */
+        this.tasks.push({
+          name: this.task,
+          status: "待辦",
+        });
+      }
+      this.task = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+.line-through {
+  text-decoration: line-through;
+}
+</style>
