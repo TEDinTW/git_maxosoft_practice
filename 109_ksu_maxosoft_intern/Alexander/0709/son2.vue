b0d292529c6a6cf1d10c9d5871b5fbf631e08f84
4070E019 <43432169+4070E019@users.noreply.github.com>
2021-07-09 17:46:51 +0800

    Add files via upload

---
+++ 109_ksu_maxosoft_intern/Alexander/0709/son2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    子组件:
+    <span>{{childValue}}</span>
+    <!-- 定义一个子组件传值的方法 -->
+    <input type="button" value="点击触发" @click="childClick">
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        childValue: '我是子组件的数据'
+      }
+    },
+    methods: {
+      childClick () {
+        // childByValue是在父组件on监听的方法
+        // 第二个参数this.childValue是需要传的值
+        this.$emit('childByValue', this.childValue)
+      }
+    }
+  }
+</script>
