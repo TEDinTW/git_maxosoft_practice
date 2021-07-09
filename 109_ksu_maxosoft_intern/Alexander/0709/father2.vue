d678ea51aeb4b4f780289b742ee9e89230b0da1a
4070E019 <43432169+4070E019@users.noreply.github.com>
2021-07-09 17:46:51 +0800

    Add files via upload

---
+++ 109_ksu_maxosoft_intern/Alexander/0709/father2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    父组件:
+    <span>{{name}}</span>
+    <br>
+    <br>
+    <!-- 引入子组件 定义一个on的方法监听子组件的状态-->
+    <child v-on:childByValue="childByValue"></child>
+  </div>
+</template>
+<script>
+  import child from './son2.vue'
+  export default {
+    components: {
+      child
+    },
+    data () {
+      return {
+        name: ''
+      }
+    },
+    methods: {
+      childByValue: function (childValue) {
+        // childValue就是子组件传过来的值
+        this.name = childValue
+      }
+    }
+  }
+</script>
+
