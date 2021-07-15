3834d4066bab55886802a94f81b95c08df120576
Mingkai880904 <43432006+Mingkai880904@users.noreply.github.com>
2021-07-15 14:30:13 +0800

    PR vue (#18)
    * add a ReadMe.txt
    
    * vue_project
    
    * Delete 109_ksu_maxosoft_intern/Mingkai/vue_project directory
    
    * vue_project
    
    * vue_project
    
    * Add files via upload
    
    * add vue-demo
    
    * Delete main.js
    
    * Delete hello.md
    
    * Delete components.vue
    
    * Delete child.vue
    
    * Delete HelloWorld.vue
    
    * Delete Hello.vue
    
    * Delete App.vue

---
+++ 109_ksu_maxosoft_intern/Mingkai/vue_project/vue-demo/src/components/chart.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineComponent } from 'vue'
+import { Bar } from 'vue3-chart-v2'
+
+
+export default defineComponent({
+  name: 'MonthlyChart',
+  extends: Bar,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ['一月', '二月', '三月', '四月', '五月', '六月'],
+      datasets: [
+        {
+          label: 'Data 1',
+          backgroundColor: '#f87979',
+          width: 500,
+          data: [40, 20, 12, 39, 70, 40]
+        },{
+          label: 'Data 2',
+          backgroundColor: '#05CBE1',
+          data: [60, 55, 32, 10, 2, 12, 53, 62,25]
+        }
+      ]
+    })
+  }
+})
+</script>
+
