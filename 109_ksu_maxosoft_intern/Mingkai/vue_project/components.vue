cb8a768796c1261dfd52f1a0068979394ba86919
Mingkai880904 <43432006+Mingkai880904@users.noreply.github.com>
2021-07-28 15:55:32 +0800

    bar and doughnut (#20)
    * add a ReadMe.txt
    
    * vue_project
    
    * Delete 109_ksu_maxosoft_intern/Mingkai/vue_project directory
    
    * vue_project
    
    * vue_project
    
    * Add files via upload
    
    * add vue-demo
    
    * 'bar.vue'
    
    * add doughnut.vue
    
    Co-authored-by: TEDinTW <yihsin2010@gmail.com>

---
+++ 109_ksu_maxosoft_intern/Mingkai/vue_project/components.vue
@@ -0,0 +1,32 @@
+  <template>
+  <div>
+    父組件:
+    <input type="text" v-model="name">
+    <!-- <span>{{name}}</span> -->
+    <br>
+    <br>
+      <child :inputName="name"></child>
+      <child :inputName="name"></child>
+      <child :inputName="name"></child>
+
+    <!-- <child v-on:childByValue="childByValue"></child> -->
+  </div>
+</template>
+<script>
+  import child from './child.vue'
+  export default {
+    components: {
+      child
+    },
+    data: function() {
+      return {
+        name: ''
+      }
+    // },
+    // methods: {
+      // childByValue: function (childValue) {
+        // this.name = childValue
+      // }
+    }
+  }
+</script>
--- 109_ksu_maxosoft_intern/Mingkai/vue_project/main.js
+++ 109_ksu_maxosoft_intern/Mingkai/vue_project/main.js
@@ -8,14 +8,9 @@ createApp(App).mount('#app')
 import Vue from 'vue'
 import components from './components/components.vue'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<MyApp></MyApp>',
   components: { components }
 })
-
