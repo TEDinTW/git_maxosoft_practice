a108d59a7da8b34a98e4b6f875ef43ac06f17506
Mingkai880904 <a0912868757@gmail.com>
2021-08-02 11:44:30 +0800

    add a App.vue

--- 109_ksu_maxosoft_intern/Mingkai/vue_project/vue-demo/src/App.vue
+++ 109_ksu_maxosoft_intern/Mingkai/vue_project/vue-demo/src/App.vue
@@ -1,65 +1,39 @@
 <template>
-	<div id="app">
-		<div class="row">
-			<!-- <div class="col" v-for="(item, index) in user" :key="index">
-				<child :data="item">
-				</child> -->
-          <child2 :data="data" option="option"> </child2>
-	
-		</div>
-	</div>
+    <div id="PdfPage2">
+        <div class="no-print">
+            <button type="button" class="btn btn-primary" @click="pdfBtn">匯出PDF</button>
+        </div>
+        <div id="pdfDom" style="padding:10px;background-color:#fff;">
+            <img alt="Vue logo" src="@/assets/logo.png">
+            <h1>Welcome to Your Vue.js App</h1>
+            <p v-for="(item,index) in 6" :key="index">{{item}}Welcome to Your Vue.js App</p>
+        </div>
+    </div>
 </template>
 
 <script>
-import child2 from "./components/child2.vue";
-
-export default {
-  name: "App",
-  components: {
-    child2,
-  },
-  data() {
-    return {
-      data: {
-        title: "A公司",
-        labels: [],
-        datasets: [
-          {
-            label: "Data One",
-            backgroundColor: "rgba(255, 99, 132, 0.2)",
-            borderColor: "rgb(255, 99, 132)",
-            data: [10, 20, 12, 39, 70, 40],
-            borderWidth: 1,
-          },
-          {
-            label: "Data One",
-            backgroundColor: "rgba(153, 102, 255, 0.2)",
-            borderColor: "rgb(153, 102, 255)",
-            data: [60, 55, 32, 10, 2, 12, 53, 62, 25],
-            borderWidth: 1,
-          },
-        ],
-      },
-      option: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    };
-  },
-};
+    export default {
+        name: "PdfPage2",
+        methods:{
+            pdfBtn(){
+                window.print();
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  padding: 1em;
+<style scoped>
+/*儲存時的樣式*/
+/*
+瞭解更多可 百度CSS print樣式
+*/
+@media print{
+    .no-print{
+        display: none;
+    }
+}
+/*列印頁配置*/
+@page{
+    margin:60px 10px;
 }
 </style>
